d3944bb75ce9898984be9afc3345d677a5cf0abc
Dhananjay Shinde <dhananjayshinde1999@gmail.com>
2021-09-22 22:19:50 +0530

    script update for zip removal

--- sonar-scanner-install.sh
+++ sonar-scanner-install.sh
@@ -5,12 +5,11 @@ wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scann
 
 echo "extracting..."
 unzip sonar-scanner-cli-4.6.2.2472-linux.zip
+rm sonar-scanner-cli-4.6.2.2472-linux.zip
 
 sudo mv sonar-scanner-cli-4.6.2.2472-linux /opt/sonar-scanner
 
 sudo sed 's/#sonar/sonar/' /opt/sonar-scanner/conf/sonar-scanner.properties > sonar-scanner.properties.temp && mv sonar-scanner.properties.temp /opt/sonar-scanner/conf/sonar-scanner.properties 
-
-#rm sonar-scanner-cli-4.6.2.2472-linux.zip
 
 echo "adding to PATH"
 echo '\nexport PATH="$PATH:/opt/sonar-scanner/bin"' >> .bashrc
